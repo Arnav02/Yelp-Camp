12a88d65625fb08b7014e54d80e191228614315c
Arnav02 <arnavadhiya@gmail.com>
2021-05-26 23:01:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const { validate } = require('./models/campground');
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404));
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh no, Something went wrong!'
+    res.status(statusCode).render('error', { err });
+})
+
+app.listen(3000, () => {
+    console.log('On port, 3000!');
+})
