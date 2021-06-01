abfb99bbd0556062a76c05b791130fca45b7fc5c
Arnav02 <arnavadhiya@gmail.com>
2021-06-01 21:45:23 +0530

    fancy clusterMap added

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -30,7 +30,7 @@ const seedDB = async () => {
             price,
             geometry: {
                 type: "Point",
-                coordinates: [-73.9866, 40.7306]
+                coordinates: [cities[random1000].longitude, cities[random1000].latitude]
             },
             images: [
                 {
