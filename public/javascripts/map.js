304a28a16a1ed62e5760a550b1fb1005400265d9
Arnav02 <arnavadhiya@gmail.com>
2021-06-02 12:21:15 +0530

    map controls added

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,6 +6,8 @@ const map = new mapboxgl.Map({
     zoom: 10
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
